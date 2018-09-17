b24aca0a5d1aa0b03eb968e2185c7bab041f9170
zEttOn86 <38281934+zEttOn86@users.noreply.github.com>
2018-09-17 23:43:43 +0900

    Add src/models/gen_models

---
+++ src/models/gen_models/resblocks.py
@@ -0,0 +1,43 @@
+import math
+import chainer
+import chainer.links as L
+import chainer.functions as F
+
+
+def _upsample(x):
+    h, w = x.shape[2:]
+    return F.unpooling_2d(x, 2, outsize=(h * 2, w * 2))
+
+
+def upsample_conv(x, conv):
+    return conv(_upsample(x))
+
+class Block(chainer.Chain):
+    def __init__(self, in_channels, out_channels, hidden_channels=None, ksize=3, pad=1,
+                activation=F.relu, upsample=False):
+        super(Block, self).__init__()
+        initializer = chainer.initializers.GlorotUniform(math.sqrt(2))
+        self.activation = activation
+        self.upsample = upsample
+        hidden_channels = out_channels if hidden_channels is None else hidden_channels
+        with self.init_scope():
+            self.c1 = L.Convolution2D(in_channels, hidden_channels, ksize=ksize, pad=pad, initialW=initializer)
+            self.c2 = L.Convolution2D(hidden_channels, out_channels, ksize=ksize, pad=pad, initialW=initializer)
+            self.b1 = L.BatchNormalization(in_channels)
+            self.b2 = L.BatchNormalization(hidden_channels)
+
+    def residual(self, x, **kwargs):
+        h = x
+        h = self.b1(h, **kwargs)
+        h = self.activation(h)
+        h = upsample_conv(h, self.c1) if self.upsample else self.c1(h)
+        h = self.b2(h, **kwargs)
+        h = self.activation(h)
+        h = self.c2(h)
+        return h
+
+    def shortcut(self, x):
+        return x
+
+    def __call__(self, x):
+        return self.residual(x, **kwargs) + self.shortcut(x)
