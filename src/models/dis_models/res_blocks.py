6a7113cd7a9bc08175e1771ecbf3a5920dccb3e6
zEttOn86 <38281934+zEttOn86@users.noreply.github.com>
2018-09-18 00:39:24 +0900

    Add src/models/dis_models/res_blocks.py

---
+++ src/models/dis_models/res_blocks.py
@@ -0,0 +1,63 @@
+#coding:utf-8
+import math
+import chainer
+import chainer.functions as F
+from source.links.sn_convolution_2d import SNConvolution2D
+
+def _downsample(x):
+    # Downsample (Mean Avg Pooling with 2x2 kernel)
+    return F.average_pooling_2d(x, 2)
+
+class Block(chainer.Chain):
+    def __init__(self, in_channels, out_channels, hidden_channels=None, ksize=3, pad=1,
+                activation=F.relu, downsample=False):
+        super(Block, self).__init__()
+        initializer = chainer.initializers.GlorotUniform(math.sqrt(2))
+        self.activation = activation
+        self.downsample = downsample
+        hidden_channels = in_channels if hidden_channels is None else hidden_channels
+        with self.init_scope():
+            self.c1 = SNConvolution2D(in_channels, hidden_channels, ksize=ksize, pad=pad, initialW=initializer)
+            self.c2 = SNConvolution2D(hidden_channels, out_channels, ksize=ksize, pad=pad, initialW=initializer)
+
+    def residual(self, x):
+        h = x
+        h = self.activation(h)
+        h = self.c1(h)
+        h = self.activation(h)
+        h = self.c2(h)
+        if self.downsample:
+            h = _downsample(h)
+        return h
+
+    def shortcut(self, x):
+        return x
+
+    def __call__(self, x):
+        return self.residual(x) + self.shortcut(x)
+
+
+class OptimizedBlock(chainer.Chain):
+    def __init__(self, in_channels, out_channels, ksize=3, pad=1, activation=F.relu):
+        super(OptimizedBlock, self).__init__()
+        initializer = chainer.initializers.GlorotUniform(math.sqrt(2))
+        initializer_sc = chainer.initializers.GlorotUniform()
+        self.activation = activation
+        with self.init_scope():
+            self.c1 = SNConvolution2D(in_channels, out_channels, ksize=ksize, pad=pad, initialW=initializer)
+            self.c2 = SNConvolution2D(out_channels, out_channels, ksize=ksize, pad=pad, initialW=initializer)
+            self.c_sc = SNConvolution2D(in_channels, out_channels, ksize=1, pad=0, initialW=initializer_sc)
+
+    def residual(self, x):
+        h = x
+        h = self.c1(h)
+        h = self.activation(h)
+        h = self.c2(h)
+        h = _downsample(h)
+        return h
+
+    def shortcut(self, x):
+        return self.c_sc(_downsample(x))
+
+    def __call__(self, x):
+        return self.residual(x) + self.shortcut(x)
