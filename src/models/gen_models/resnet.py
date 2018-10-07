0f687939955ff5f9b1415512941e2b5e82bd9b07
zEttOn86 <38281934+zEttOn86@users.noreply.github.com>
2018-10-07 15:42:11 +0900

    Update  src/models/gen_models/resnet.py

--- src/models/gen_models/resnet.py
+++ src/models/gen_models/resnet.py
@@ -5,7 +5,7 @@ from gen_models.resblocks import Block
 from source.miscs.random_samples import sample_continuous
 
 class ResNetGenerator(chainer.Chain):
-    def __init__(self, ch=32, dim_z=128, bottom_width=4, activation=F.relu, distribution="normal"):
+    def __init__(self, ch=64, dim_z=128, bottom_width=4, activation=F.relu, distribution="normal"):
         super(ResNetGenerator, self).__init__()
         initializer = chainer.initializers.GlorotUniform()
         self.bottom_width = bottom_width
@@ -13,15 +13,15 @@ class ResNetGenerator(chainer.Chain):
         self.distribution = distribution
         self.dim_z = dim_z
         with self.init_scope():
-            self.l1 = L.Linear(dim_z, (bottom_width ** 2) * ch * 32, initialW=initializer)
-            self.block2 = Block(ch * 32, ch * 32, activation=activation, upsample=True) #(4x4) => (8x8)
-            self.block3 = Block(ch * 32, ch * 16, activation=activation, upsample=True) #(8x8) => (16x16)
-            self.block4 = Block(ch * 16, ch * 8, activation=activation, upsample=True) #(16x16) => (32x32)
-            self.block5 = Block(ch * 8, ch * 4, activation=activation, upsample=True) #(32x32) => (64x64)
-            self.block6 = Block(ch * 4, ch * 2, activation=activation, upsample=True) #(64x64) => (128x128)
-            self.block7 = Block(ch * 2, ch, activation=activation, upsample=True) #(128x128) => (256x256)
-            self.b7 = L.BatchNormalization(ch)
-            self.l7 = L.Convolution2D(ch, 3, ksize=3, stride=1, pad=1, initialW=initializer)
+            self.l1 = L.Linear(dim_z, (bottom_width ** 2) * ch * 16, initialW=initializer)
+            self.block2 = Block(ch * 16, ch * 16, activation=activation, upsample=True) #(4x4) => (8x8)
+            self.block3 = Block(ch * 16, ch * 8, activation=activation, upsample=True) #(8x8) => (16x16)
+            self.block4 = Block(ch * 8, ch * 4, activation=activation, upsample=True) #(16x16) => (32x32)
+            self.block5 = Block(ch * 4, ch * 2, activation=activation, upsample=True) #(32x32) => (64x64)
+            self.block6 = Block(ch * 2, ch, activation=activation, upsample=True) #(64x64) => (128x128)
+            self.block7 = Block(ch, ch//2, activation=activation, upsample=True) #(128x128) => (256x256)
+            self.b7 = L.BatchNormalization(ch//2)
+            self.l7 = L.Convolution2D(ch//2, 3, ksize=3, stride=1, pad=1, initialW=initializer)
 
     def __call__(self, batchsize=64, z=None, **kwargs):
         if z is None:
@@ -41,7 +41,10 @@ class ResNetGenerator(chainer.Chain):
         print('block5 {}'.format(h.shape))
         h = self.block6(h)
         print('block6 {}'.format(h.shape))
+        h = self.block7(h)
+        print('block7 {}'.format(h.shape))
         h = self.b7(h)
         h = self.activation(h)
         h = F.tanh(self.l7(h))
+        print('output {}'.format(h.shape))
         return h
