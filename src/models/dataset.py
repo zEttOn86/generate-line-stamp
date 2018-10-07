2fa3bb48e98dd99e4af764298978de384c49b93b
zEttOn86 <38281934+zEttOn86@users.noreply.github.com>
2018-10-07 14:58:14 +0900

    Update src/models/dataset.py

--- src/models/dataset.py
+++ src/models/dataset.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 import chainer
 import glob
-from itertools import chain
+
 class StampDataset(chainer.dataset.DatasetMixin):
     def __init__(self, root, augmentation=False):
         print(' Initialize dataset')
@@ -24,12 +24,13 @@ class StampDataset(chainer.dataset.DatasetMixin):
     def transform(self, image):
         # Random right left transform
         if np.random.rand() > 0.5:
-            img = img[:, :, ::-1]
-        img += np.random.uniform(size=img.shape, low=0, high=1./128)
-        return img
+            image = image[:, :, ::-1]
+        image += np.random.uniform(size=image.shape, low=0, high=1./128)
+        return image
 
     def get_example(self, i):
-        img = Image.open(self._fnames[i])
+        img = Image.open(self._fnames[i]).convert('RGBA').convert('RGB')
+        print(self._fnames[i])
         # Normalization [-1,1]
         img = np.asarray(img).astype(np.float32).transpose(2,0,1)/128.0-1. # (ch, y, x)
         if not self._augmentation:
