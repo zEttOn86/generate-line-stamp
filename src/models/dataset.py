6e606fcb18e7d9de0cafbf9b561eefb0d3d80c5f
zEttOn86 <38281934+zEttOn86@users.noreply.github.com>
2018-10-07 11:52:48 +0900

    Update src/models/dataset.py

--- src/models/dataset.py
+++ src/models/dataset.py
@@ -13,12 +13,6 @@ class StampDataset(chainer.dataset.DatasetMixin):
 
         # Get image file path
         fnames = [os.path.abspath(i) for i in glob.glob("{}/**/*.png".format(self._root), recursive=True)]
-        #dnames = glob.glob('{}/*'.format(self._root))
-        #fnames = [glob.glob('{}/*.png'.format(d)) for d in dnames]
-        print(len(fnames))
-        #fnames = list(chain.from_iterable(fnames))
-        #print(len(fnames[0]))
-        print(fnames)
 
         self._fnames = fnames
 
